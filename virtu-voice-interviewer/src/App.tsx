408efe8fdc8b3a356bce29ab8030eeb40598ffec
Jose <jsalazar@softpymes.com.co>
2025-05-14 11:46:16 -0500

    Fix dark theme implementation

--- virtu-voice-interviewer/src/App.tsx
+++ virtu-voice-interviewer/src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/hooks/useAuth";
-import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ThemeProvider } from "@/components/theme-provider";
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import NotFound from "./pages/NotFound";
